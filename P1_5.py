ac37dc67645458535dd6264ca37df3441a9f6943
mariorodriguezmiguel <mario.rodriguez25@estudiantes.uva.es>
2026-09-22 09:39:25 +0200

    Format output of center of gravity coordinates
    Updated the output format of the center of gravity coordinates to display two decimal places for XG and three decimal places for YG.

--- P1_5.py
+++ P1_5.py
@@ -1,4 +1,3 @@
-
 # CÁLCULO DEL CENTRO DE GRAVEDAD DE 3 MASAS PUNTUALES
 
 x1 = float(input("Coordenada x del punto 1:"))
@@ -18,4 +17,6 @@ M = m1 + m2 + m3
 XG = (m1 * x1 + m2 * x2 + m3 * x3) / M
 YG = (m1 * y1 + m2 * y2 + m3 * y3) / M
 
-print(f"Las coordenadas del centro de gravedad son ({XG},{YG})")
+print(f"Las coordenadas del centro de gravedad son ({XG:.2f},{YG:.3f})")
+# :.2f PARA PONER DOS Nº DECIMALES 
+# :.3f PARA PONER TRES Nº DECIMALES 
